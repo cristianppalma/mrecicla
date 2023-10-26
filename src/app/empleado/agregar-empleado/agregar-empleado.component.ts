5eed4877baae56c815eb5a3fc7d480812aa32d0c
PandaLobito <146490549+PandaLobito@users.noreply.github.com>
2023-10-26 12:42:44 -0600

    patnalla de agregar empleados lista

--- src/app/empleado/agregar-empleado/agregar-empleado.component.ts
+++ src/app/empleado/agregar-empleado/agregar-empleado.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+interface puesto {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-agregar-empleado',
   templateUrl: './agregar-empleado.component.html',
   styleUrls: ['./agregar-empleado.component.css']
 })
 export class AgregarEmpleadoComponent {
+  
+  puestos: puesto[] = [
+    {value: 'Operador-costura-0', viewValue: ' Operador de costura circular'},
+    {value: 'Supervisor-1', viewValue: 'Supervisor'},
+    {value: 'Mecanico-2', viewValue: 'Mecanico GRAL'},
+    {value: 'Hilador-2', viewValue: 'Hilador'},
+  ];
 
 }
--- src/app/empleado/editar-empleado/editar-empleado.component.ts
+++ src/app/empleado/editar-empleado/editar-empleado.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+interface puesto {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-editar-empleado',
   templateUrl: './editar-empleado.component.html',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class EditarEmpleadoComponent {
 
+  puestos: puesto[] = [
+    {value: 'Operador-costura-0', viewValue: ' Operador de costura circular'},
+    {value: 'Supervisor-1', viewValue: 'Supervisor'},
+    {value: 'Mecanico-2', viewValue: 'Mecanico GRAL'},
+    {value: 'Hilador-2', viewValue: 'Hilador'},
+  ];
 }
+
+
+
