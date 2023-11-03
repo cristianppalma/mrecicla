d9be55270441c6a3c038b2a94de3e4944452f457
PandaLobito <146490549+PandaLobito@users.noreply.github.com>
2023-11-03 12:48:19 -0600

    funcionalidad de perfil administrador casi-lista

--- src/app/empleado/perfil-admin/perfil-admin.component.ts
+++ src/app/empleado/perfil-admin/perfil-admin.component.ts
@@ -1,22 +1,102 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AvisoDialogComponent } from '../aviso-dialog/aviso-dialog.component'
+import { MatDialog } from '@angular/material/dialog';
+import { EmpleadoService } from '../empleado.service';
+import { ActivatedRoute } from '@angular/router';
+import { __param } from 'tslib';
 
-
-interface puesto {
-  value: string;
-  viewValue: string;
-}
 @Component({
   selector: 'app-perfil-admin',
   templateUrl: './perfil-admin.component.html',
   styleUrls: ['./perfil-admin.component.css']
 })
-export class PerfilAdminComponent {
+export class PerfilAdminComponent implements OnInit {
+  formularioEditarEmpleado: FormGroup;
+  idEmpleado: any;
+
+  constructor(
+    private editarEmpleado: EmpleadoService,
+    private FormBuilder: FormBuilder,
+    private activateRoute : ActivatedRoute,
+    private dialog: MatDialog, 
+    private router: Router,
+
+
+    ) {
+      this.formularioEditarEmpleado =this.FormBuilder.group({
+        Nombre:['', [Validators.required]],
+        ApellidoPaterno: ['', [Validators.required]],
+        Correo:['', [Validators.required]],
+        Pass:['', [Validators.required]],
+        ApellidoMaterno:['', [Validators.required]],
+        Turno:['', [Validators.required]],
+        Sueldo:['', [Validators.required]],
+        Area :[''],
+        Puesto:[''],
+        Domicilio:['']
+      });
+
+      this.activateRoute.paramMap.subscribe(() =>{
+
+        this.idEmpleado = localStorage.getItem("id_user")
+        
+        this.editarEmpleado.EditarEmpleado(this.idEmpleado).subscribe((respuesta=>{
+        
+           this.formularioEditarEmpleado.setValue({
+            Nombre: respuesta.Nombre,
+            ApellidoPaterno: respuesta.ApellidoPaterno,
+            Correo: respuesta.Correo,
+            Pass: respuesta.Pass,
+            ApellidoMaterno: respuesta.ApellidoMaterno,
+            Turno: respuesta.Turno,
+            Sueldo: respuesta.Sueldo,
+            Area: respuesta.Area,
+            Puesto: respuesta.Puesto,
+            Domicilio: respuesta.Domicilio,
+           }); 
+         } ))
+
+      })
+     }
+     enviarDatos(): void {
+      if (this.formularioEditarEmpleado.valid) {
+        console.log('Se presionó el botón');
+        console.log(this.formularioEditarEmpleado.value);
+        this.editarEmpleado.UpdateEmpleado(this.formularioEditarEmpleado.value,this.idEmpleado).subscribe(
+          (response) => {
+           console.log('Se registro correctamente');
+           this.mostratDialogoAviso();
+            
+            
+          },
+          (error) => {
+            console.log('Aqui trono');
+  
+            // Manejar errores del servicio aquí
+          }
+        );
+      }
+    }
+    mostratDialogoAviso():void{
+      console.log('Tumadre');
+      const dialogAviso = this.dialog.open(AvisoDialogComponent,{
+        data: {message: 'Se Actualizo correctamente el Empleado'}
+      });
+      dialogAviso.afterClosed().subscribe(result => {
+        if (result) {
+          this.router.navigateByUrl('/dashboard/tablero');
+        }
+      });
+    
+    }
+  
+    ngOnInit(): void {
+    
+    }
+    
+}
 
   
-  puestos: puesto[] = [
-    {value: 'Operador-costura-0', viewValue: ' Operador de costura circular'},
-    {value: 'Supervisor-1', viewValue: 'Supervisor'},
-    {value: 'Mecanico-2', viewValue: 'Mecanico GRAL'},
-    {value: 'Hilador-2', viewValue: 'Hilador'},
-  ];
-}
