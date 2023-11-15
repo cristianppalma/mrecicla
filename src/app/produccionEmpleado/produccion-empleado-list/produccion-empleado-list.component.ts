ae6404d9aefbdbf380bea12b6cbaa602c53b54ab
Cristian Pérez Palma <cristianshawn82@gmail.com>
2023-11-15 12:53:21 -0600

    Se comenzo a agregar la lista de registros v2

--- src/app/produccionEmpleado/produccion-empleado-list/produccion-empleado-list.component.ts
+++ src/app/produccionEmpleado/produccion-empleado-list/produccion-empleado-list.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
+import { ProduccionEmpleadoService } from '../produccion-empleado.service';
+
+// import { PeriodicElement } from '../PeriodicElement';
 
 @Component({
   selector: 'app-produccion-empleado-list',
---
+++ src/app/produccionEmpleado/produccion.ts
@@ -0,0 +1,7 @@
+export class Produccion{
+  // name_proveedor!:String;
+  // producto_proveedor!:String;
+  // direccion_proveedor!:String;
+  // rfc_proveedor!:String;
+  // description_proveedor!:String;
+}
