937d7a463195311f45f6873e960fe083538d319f
Cristian Pérez Palma <cristianshawn82@gmail.com>
2023-12-22 16:09:39 -0600

    css en movil en index y auth

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AvisoDialogComponent } from 'src/app/maquinas/aviso-dialog/aviso-dialog.component'
 import { MatDialog } from '@angular/material/dialog';
 import { AvisoLoginComponent } from '../aviso-login/aviso-login.component';
 
